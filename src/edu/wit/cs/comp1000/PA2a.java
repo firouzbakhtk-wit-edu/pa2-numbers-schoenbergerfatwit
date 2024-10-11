0d0a430ebb04c031f19845e0e1453ac78350fd89
Finn Schoenberger <schoenbergerf@wit.edu>
2024-10-10 22:38:53 -0400

    PA2a and PA2b

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,102 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+
+/**
+ * Solution to the PA2a programming assignment.
+ * When it runs it asks the user for 5 numbers
+ * and then outputs the sum and average of the positive
+ * and negative numbers and the sum and of all the numbers.
+ * 
+ * @author Finn Schoenberger
+ */
 public class PA2a {
 
-	// TODO: document this function
+	/**
+	 * Starts the program and asks the user for
+	 * 5 numbers, outputting different solutions.
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		double[] numbers = new double[5];
+		
+		System.out.printf("Enter five whole numbers: ");
+		for (int i = 0; i < 5; i++) {
+			numbers[i] = input.nextDouble();
+		}
+		
+		int posNum = 0;
+		double posSum = 0;
+		for (int i = 0; i < 5; i++) {
+			if (numbers[i] >= 1) {
+				posSum += numbers[i];
+				posNum += 1;
+			}
+		}
+		
+		if (posNum == 1) {
+			System.out.printf("The sum of the %d positive number: %.0f%n", posNum, posSum);
+		} else {
+			System.out.printf("The sum of the %d positive numbers: %.0f%n", posNum, posSum);
+		}
+
+		
+		int negNum = 0;
+		double negSum = 0;
+		for (int i = 0; i < 5; i++) {
+			if (numbers[i] <= 0) {
+				negSum += numbers[i];
+				negNum += 1;
+			}
+		}
+		
+		if (negNum == 1) {
+			System.out.printf("The sum of the %d non-positive number: %.0f%n", negNum, negSum);
+		} else {
+			System.out.printf("The sum of the %d non-positive numbers: %.0f%n", negNum, negSum);
+		}
+
+		double sum = 0;
+		for (int i = 0; i < 5; i++) {
+			sum += numbers[i];
+		}
+		System.out.printf("The sum of the 5 numbers: %.0f%n", sum);
+		
+		double posAverage = 0;
+		if (posNum == 0) {
+			posAverage = 0.00;
+		} else {
+			posAverage = posSum / posNum;
+		}
+		
+		if (posNum == 1) {
+			System.out.printf("The average of the %d positive number: %.2f%n", posNum, posAverage);
+		} else {
+			System.out.printf("The average of the %d positive numbers: %.2f%n", posNum, posAverage);
+		}
+
+		
+		double negAverage = 0;
+		if (negNum == 0) {
+			negAverage = 0.00;
+		} else {
+			negAverage = negSum / negNum;
+		}
+		
+		if (negNum == 1) {
+			System.out.printf("The average of the %d non-positive number: %.2f%n", negNum, negAverage);
+		} else {
+			System.out.printf("The average of the %d non-positive numbers: %.2f%n", negNum, negAverage);
+		}
+
+		
+		double average = sum / 5;
+		System.out.printf("The average of the 5 numbers: %.2f%n", average);
+		
+		input.close();
+		
 	}
 
 }
--- src/edu/wit/cs/comp1000/PA2b.java
+++ src/edu/wit/cs/comp1000/PA2b.java
@@ -1,11 +1,46 @@
 package edu.wit.cs.comp1000;
+import java.util.Scanner;
 
-// TODO: document this class
+/**
+ * Solution to the PA2b programming assignment.
+ * When it runs it asks the user for 3 numbers a,
+ * b, and c and then takes the discriminant and 
+ * outputs the root(s).
+ * 
+ * @author Finn Schoenberger
+ */
 public class PA2b {
 
-	// TODO: document this function
+	/**
+	 * Starts the program and asks the user for
+	 * 3 numbers, outputting their roots from the
+	 * discriminant.
+	 * 
+	 * @param args command-line arguments, ignored
+	 */
 	public static void main(String[] args) {
-		// TODO: write your code here
+		Scanner input = new Scanner(System.in);
+		
+		double a, b, c;
+		System.out.printf("Enter a b c: ");
+		a = input.nextDouble();
+		b = input.nextDouble();
+		c = input.nextDouble();
+		
+		double discriminant = (b*b) - 4*(a*c);
+		
+		if (discriminant < 0) {
+			System.out.printf("Roots: imaginary%n");
+		} else if (discriminant == 0) {
+			double root = ((-1*b)/(2*a));
+			System.out.printf("Root: %.2f%n", root);
+		} else if (discriminant > 0) {
+			double root1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            System.out.printf("Roots: %.2f, %.2f%n", Math.min(root1, root2), Math.max(root1, root2));
+		}
+		
+		
 	}
 
 }
